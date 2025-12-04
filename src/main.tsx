3343a2b4bce3bbb0b864d5029ae29f23c62c8348
justis <justisstewart7@gmail.com>
2025-12-04 09:09:38 -0500

    switch frontend to mainnet-beta

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./index.css";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+import { clusterApiUrl } from "@solana/web3.js";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+
+// MAINNET ENDPOINT
+const endpoint = clusterApiUrl("mainnet-beta");
+
+// Phantom wallet adapter
+const wallets = [new PhantomWalletAdapter()];
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <App />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   </React.StrictMode>
 );
